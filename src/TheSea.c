9f66baae78696c1fae5395c4d4af33d93ec949f2
bonoj <johnlbono@gmail.com>
2017-07-27 21:02:54 -0500

    Initial commit

---
+++ src/TheSea.c
@@ -0,0 +1,25 @@
+/*
+ ============================================================================
+ Name        : TheSea.c
+ Author      : 
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+
+	char *p = "Hello World";
+
+	//puts("!!!Hello World!!!"); /* prints !!!Hello World!!! */
+
+	printf("%s\n", p);
+
+	printf("%d\n", EXIT_SUCCESS);
+
+	return EXIT_SUCCESS;
+}
